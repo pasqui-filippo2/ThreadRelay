1a4c3958fde588ac0345b0785e220f2e94a7e8a2
pasqui-filippo2 <pasqui.filippo@franchettisalviani.net>
2026-05-04 09:13:22 +0200

    Creazione pattern Observer Subject
    -Creazione delle interfacce per l'aggiornamento della grafica

---
+++ src/threadrelay/Subject.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package threadrelay;
+
+/**
+ *
+ * @author pasqui.filippo
+ */
+public interface Subject {
+    public void addObserver(Observer o);
+    public void removeObserver(Observer o);
+    public void notifyObserver(Observer o);
+}
